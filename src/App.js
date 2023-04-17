019ec4bc5e6322e877c9c9cda95c66e6e994ce60
Nirav9603 <niravmagatarapara111@gmail.com>
2023-04-17 15:35:18 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 
 function App() {
+
+  const [Data, setData] = useState([]);
+
+  useEffect(() => {
+    getData()
+  }, []);
+
+  const getData = () => {
+    axios({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/users'
+    })
+      .then((response) => {
+        setData(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped bordered>
+        <thead style={{background: "darkblue", color: "white"}}>
+          <tr>
+            <th>Name</th>
+            <th>Username </th>
+            <th>Email</th>
+            <th>City</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Compamy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Data.map((data, index) => {
+              return(
+                <tr key={index}>
+                <td>{data.name}</td>
+                <td>{data.username}</td>
+                <td>{data.email}</td>
+                <td>{data.address.city}</td>
+                <td>{data.phone}</td>
+                <td>{data.website}</td>
+                <td>{data.company.name}</td>
+              </tr>
+              );
+            })
+          }
+        </tbody>
+      </Table>
     </div>
   );
 }
